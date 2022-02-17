6f74119f8a09e809ee95d9049e49f245fa4c91df
iOSayedAhmed <elsayed1ahmed0@gmail.com>
2022-02-17 10:03:26 +0200

    Initial Commit

---
+++ AlamofirePostRequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofirePostRequest
+//
+//  Created by Develop on 2/17/22.
+//  Copyright © 2022 Develop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
